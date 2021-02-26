726466f817c63b517185289e2edbe579cdaf82ad
OlegZhdanov <stalker08@gmail.com>
2021-02-26 22:31:07 +0500

    first steps

--- client.py
+++ client.py
@@ -3,10 +3,33 @@ from socket import *
 import time
 
 
+class Client:
+    def __init__(self, account_name, password, status):
+        self.account_name = account_name
+        self.password = password
+        self.status = status
+
+    def authenticate(self):
+        return {
+            "action": "authenticate",
+            "time": time.time(),
+            "user": {
+                    "account_name":  self.account_name,
+                    "password":      self.password
+            }
+        }
+
+    def disconnect(self):
+        return {
+            "action": "quit"
+        }
+
+
 @click.command()
 @click.argument('address', default="localhost")
 @click.argument('port', default=7777)
 def start(address, port):
+    user = Client('ivanov', '123', 'online')
     with socket(AF_INET, SOCK_STREAM) as s:  # Создает сокет TCP
         s.connect((address, port))  # Присваивает адрес и порт
         tm = s.recv(640)
--- server.py
+++ server.py
@@ -3,17 +3,49 @@ from socket import *
 import time
 from contextlib import closing
 
-ADDRESS = 'localhost'
-PORT = 7777
+ENCODING = 'utf-8'
+
+
+class Server:
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
+
+    def authenticate(self, username, password):
+        result_auth = self.check_pwd(username, password)
+        if result_auth == 200:
+            return {
+                "response": 200,
+                "alert": "добро пожаловать в чат"
+            }
+        elif result_auth == 402:
+            return {
+                "response": 402,
+                "error": "This could be wrong password or no account with that name"
+            }
+        elif result_auth == 409:
+            return {
+                "response": 409,
+                "error": "Someone is already connected with the given user name"
+            }
+
+    def check_pwd(self, username, password):
+        return 200
+
+    def client_disconnect(self, username):
+        pass
+
+    def event_handler(self, action):
+        pass
+
 
 @click.command()
 @click.argument('address', default="localhost")
 @click.argument('port', default=7777)
 def start(address, port):
-    ADDRESS, PORT = address, port
     print(address, port)
     with socket(AF_INET, SOCK_STREAM) as s:  # Создает сокет TCP
-        s.bind((ADDRESS, PORT))  # Присваивает адрес и порт
+        s.bind((address, port))  # Присваивает адрес и порт
         s.listen(5)  # Переходит в режим ожидания запросов;
         # одновременно обслуживает не более 5 запросов.
         while True:
@@ -21,10 +53,9 @@ def start(address, port):
             with closing(client) as cl:
                 print("Получен запрос на соединение от %s" % str(addr))
                 timestr = time.ctime(time.time()) + "\n"
-                client.send(timestr.encode('utf-8'))
+                client.send(timestr.encode(ENCODING))
 
-# print(ADDRESS, PORT)
+
 if __name__ == '__main__':
 
     start()
-    # print(ADDRESS, PORT)
