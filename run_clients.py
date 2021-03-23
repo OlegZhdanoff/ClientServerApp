93a534b251462894b20592d2c540bd15391a0bbc
OlegZhdanov <stalker08@gmail.com>
2021-03-23 15:38:58 +0500

    script for run multiple clients - run_clients.py

---
+++ run_clients.py
@@ -0,0 +1,31 @@
+import platform
+import subprocess
+
+import click
+
+
+@click.command()
+@click.argument('n', default=1)
+def start(n):
+    if platform.system() == 'Windows':
+        creation_flags = subprocess.CREATE_NEW_CONSOLE
+    else:
+        creation_flags = 0
+
+    processes = []
+    for i in range(n):
+        processes.append(subprocess.Popen(["python", "chat_client.py", f"--username=ivanov{i}"],
+                                          creationflags=creation_flags))
+
+    while True:
+        command = input('Command list:\t'
+                        'q - exit\t')
+        if command == 'q':
+            break
+
+    for proc in processes:
+        proc.wait()
+
+if __name__ == '__main__':
+
+    start()
