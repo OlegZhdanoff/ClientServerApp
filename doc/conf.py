22764ac3e474a9482a61f5dc8c03bad80602accf
OlegZhdanov <stalker08@gmail.com>
2021-04-28 13:00:23 +0500

    created documentation for project

--- doc/conf.py
+++ doc/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 import sys
-sys.path.insert(0, 'f:\\GeekBrains\\ClientServerApps')
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, base_dir)
 
 
 def skip(app, what, name, obj, would_skip, options):
